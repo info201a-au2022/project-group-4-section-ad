b655451950df79b2f4793805fc0ecf9438b843d6
pinaple12 <114948287+pinaple12@users.noreply.github.com>
2022-11-14 18:16:21 -0800

    Add files via upload
    done with my p2 - Justin

--- source/summary.R
+++ source/summary.R
@@ -1,1 +1,20 @@
-#summary
+# This file calculates a few summary statistics
+
+# Loading tidyverse package
+library(tidyverse)
+
+# Loading dataset
+gym_data <- read.csv("data/data.csv")
+
+
+# A list containing summary information regarding our dataset
+summary_data <- list(
+  num_observations = nrow(gym_data),
+  num_metrics = ncol(gym_data),
+  max_crowdedness = max(gym_data$number_people),
+  average_crowdedness = mean(gym_data$number_people),
+  average_temperature = mean(gym_data$temperature)
+)
+
+
+
