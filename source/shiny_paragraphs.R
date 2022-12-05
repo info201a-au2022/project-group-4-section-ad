44167322f17257bbd39b7b28634493e3e5ae2daa
wkxdavid <dpham722@uw.edu>
2022-12-05 09:57:58 -0800

    shiny info commit

--- source/shiny_paragraphs.R
+++ source/shiny_paragraphs.R
@@ -11,10 +11,15 @@ affiliation <- "INFO-201: Technical Foundations of Informatics - The Information
 
 index_report <- "Our group project revolves around gym crowdiness"
 
-research_question <- "temp"
+research_question <- "Does temperature affect how busy the gym is?
+                      Does an holiday or special event affect how busy the gym is?
+                      How busy is the gym during each day of the week?"
 
-data_set <- "This dataset is https://www.kaggle.com/datasets/nsrose7224/crowdedness-at-the-campus-gym"
-
+data_set <- "This dataset is Crowdedness at the Campus Gym (https://www.kaggle.com/datasets/nsrose7224/crowdedness-at-the-campus-gym).
+             Our dataset contains information from UC Berkeley on how crowded their gym is at a certain time
+             and information during that time that includes temperature, day of week, and relation to holiday.
+             The dataset marks the information every 10 minutes and contains over 60,000 lines.
+             "
 expected_implications <- "temp"
 
 agg_table_report1 <- "temp"
