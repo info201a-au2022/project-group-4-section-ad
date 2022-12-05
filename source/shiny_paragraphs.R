0d76ee704b81e2b33dc303c9bd94df7ccb48bb73
wkxdavid <dpham722@uw.edu>
2022-12-05 01:07:07 -0800

    shinypara

--- source/shiny_paragraphs.R
+++ source/shiny_paragraphs.R
@@ -2,3 +2,41 @@
 
 # intro
 introduction <- "test"
+
+# report page
+
+authors <- "David Pham, Justin Dong"
+
+affiliation <- "INFO-201: Technical Foundations of Informatics - The Information School - University of Washington"
+
+index_report <- "Our group project revolves around gym crowdiness"
+
+research_question <- "temp"
+
+data_set <- "This dataset is https://www.kaggle.com/datasets/nsrose7224/crowdedness-at-the-campus-gym"
+
+expected_implications <- "temp"
+
+agg_table_report1 <- "temp"
+
+agg_table_report2 <- "temp"
+
+agg_table_report3 <- "temp"
+
+agg_table_report4 <- "temp"
+
+agg_table_report5 <- "temp"
+
+findings <- "temp"
+
+discussion <- "temp"
+
+conclusion <- "temp"
+# summary page
+summary_takeaways <- "temp"
+
+summary_takeaways2 <- "temp"
+
+summary_takeaways3 <- "temp"
+
+summary_takeaways4 <- "temp"
