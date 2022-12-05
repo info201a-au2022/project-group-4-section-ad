321ccf0e650959b43fb53da43352f1e7cd5e6011
wkxdavid <dpham722@uw.edu>
2022-12-05 09:38:46 -0800

    ui interactive p

--- source/ui.R
+++ source/ui.R
@@ -24,20 +24,24 @@ introduction <- tabPanel(
   titlePanel("How Crowded is the IMA?"),
   p("Have you ever wanted to get into working out, but always thought that the IMA was too intimidating?
     According to the New York post, almost 50% of Americans are intimidated by the prospect of working out
-    at a public gym. This project seeks to alleviate this issue for University of Washington students by answering the following question: How many people 
+    at a public gym. This project seeks to alleviate this issue for University of Washington students by answering the following question: How many people
     are at the IMA at any given time? To answer this question, we are analyzing data gathered on how crowded Berkeley's campus gym is throughout the year.
-    We hope that by answering this topic, we're able to help students new to the IMA find less crowded times 
-    to workout and get started. Additionally, even seasoned gym-goers will be able to use our project to find the 
+    We hope that by answering this topic, we're able to help students new to the IMA find less crowded times
+    to workout and get started. Additionally, even seasoned gym-goers will be able to use our project to find the
     optimal time to work out for maximum efficiency."),
 )
 interactive_1 <- tabPanel(
   "Interactive"
+  p("The first interactive is planned to be an heat map that shows days of week against and how crowded
+     the gym is.")
 )
 interactive_2 <- tabPanel(
   "Interactive"
+  p("The second interactive is planned to focus on the time of day and how busy the gym crowded the gym is")
 )
 interactive_3 <- tabPanel(
   "Interactive"
+  p("The third interactive is planned to focus on how temperature affects the crowded the gym is.")
 )
 takeaways <- tabPanel(
   "Takeaways"
