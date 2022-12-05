5b99fab894175b0ba383350614d24a55c8e61142
wkxdavid <dpham722@uw.edu>
2022-12-05 01:14:47 -0800

    ui commit

--- source/ui.R
+++ source/ui.R
@@ -19,9 +19,81 @@ library(shinycssloaders)
 
 introductory_page <- tabPanel("Introduction",
 
-  titlePanel("Project Introduction: Gym Crowdiness"),
+  titlePanel("Project Introduction: Gym Crowdedness"),
 
   p(introduction),
 
   br(), br()
+)
+
+# ---------- REPORT PAGE ----------
+report_page <- tabPanel("Report",
+  h3("Gym Crowdedness"),
+  br(),
+
+  p(date), br(),
+  p(authors), br(),
+  p(affiliation), br(),
+  br(),
+
+
+  h3("Our Process"), br(),
+
+  tabsetPanel(
+    tabPanel("Index", br(), br(),
+      p(index.rmd_report),
+      p(research_questions),
+      p(data_set),
+      p(expected_implications),
+      p(findings),
+      p(discussion),
+      p(conclusion),
+      br(),
+    ),
+
+    tabPanel("Aggregate Table", br(), br(),
+      p(agg_table_report1),
+      p(agg_table_report2), br(), br(),
+      DT::dataTableOutput("agg_table"),
+      br(),
+      br(),
+      p(agg_table_report3),
+      p(agg_table_report4),
+      p(agg_table_report5),
+      br(),
+      br()
+    )
+  )
+)
+
+
+# ---------- SUMMARY PAGE ----------
+summary_page <- tabPanel("Summary",
+
+  h3("Major Takeaways"), br(),
+
+  tabsetPanel(
+    tabPanel("Takeaways",
+      br(),
+      p(summary_takeaways),
+      p(summary_takeaways2),
+      p(summary_takeaways3),
+    ),
+  )
+)
+
+
+# ---------- INTERACTIVE PAGE 1 ----------
+interactive_page_1 <- tabPanel("Temperature",
+
+  h3("temp"),
+  br(),
+)
+
+
+# ---------- INTERACTIVE PAGE 2 ----------
+interactive_page_2 <- tabPanel("Holiday",
+
+  h3("temp"),
+  br(),
 )
